3b1e2c6e25156c396f45ed08997789f84ff8d2ba
lee44 <jlee7772@gmail.com>
2023-01-19 15:16:59 -0800

    Fixed menu text size and added logo

--- components/nav/Navigation.js
+++ components/nav/Navigation.js
@@ -1,33 +1,35 @@
 import React, { useState } from 'react'
 import Menu from './menu'
 
-const Nav = ({ menu,toggleMenu, scrollToHome, scrollToAbout, scrollToSkills, scrollToPortfolio, scrollToContact }) => {
+const Nav = ({ menu, toggleMenu, scrollToHome, scrollToAbout, scrollToSkills, scrollToPortfolio, scrollToContact }) => {
   return (
     <>
       <nav className='md:container h-20 top-0 px-8 flex justify-between items-center text-white'>
-        <span>Logo</span>
+        <h1 className='bg-gradient-to-r from-violet-500 to-fuchsia-500 text-transparent bg-clip-text'>JL</h1>
         <ol className='hidden md:flex'>
           <li className='p-6 cursor-pointer' onClick={scrollToHome}>
-            Home
+            <h4>Home</h4>
           </li>
           <li className='p-6 cursor-pointer' onClick={scrollToAbout}>
-            About
+            <h4>About</h4>
           </li>
           <li className='p-6 cursor-pointer' onClick={scrollToSkills}>
-            Skills
+            <h4>Skills</h4>
           </li>
           <li className='p-6 cursor-pointer' onClick={scrollToPortfolio}>
-            Portfolio
+            <h4>Portfolio</h4>
           </li>
           <li className='p-6 cursor-pointer' onClick={scrollToContact}>
-            Contact
+            <h4>Contact</h4>
           </li>
-          <li className='p-6 cursor-pointer'>Resume</li>
+          <li className='p-6 cursor-pointer'>
+            <h4>Resume</h4>
+          </li>
         </ol>
         <div className='md:hidden space-y-2 cursor-pointer' onClick={toggleMenu}>
-          <span className='block w-5 h-0.5 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
-          <span className='block w-8 h-0.5 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
-          <span className='block w-8 h-0.5 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
+          <span className='block w-4 h-1 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
+          <span className='block w-8 h-1 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
+          <span className='block w-4 h-1 ml-auto bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
         </div>
         <Menu
           menu={menu}
