72660f2930e33b5d490a07834cf25c66d59a88a6
lee44 <jlee7772@gmail.com>
2023-01-19 17:09:59 -0800

    Added menu item component

--- components/nav/Navigation.js
+++ components/nav/Navigation.js
@@ -1,30 +1,19 @@
 import React, { useState } from 'react'
 import Menu from './menu'
+import MenuItem from './menuItem'
 
 const Nav = ({ menu, toggleMenu, scrollToHome, scrollToAbout, scrollToSkills, scrollToPortfolio, scrollToContact }) => {
   return (
     <>
       <nav className='md:container h-14 top-0 px-8 flex justify-between items-center text-white'>
         <h2 className='bg-gradient-to-r from-violet-500 to-fuchsia-500 text-transparent bg-clip-text'>JL</h2>
-        <ol className='hidden md:flex'>
-          <li className='p-6 cursor-pointer' onClick={scrollToHome}>
-            <h5>Home</h5>
-          </li>
-          <li className='p-6 cursor-pointer' onClick={scrollToAbout}>
-            <h5>About</h5>
-          </li>
-          <li className='p-6 cursor-pointer' onClick={scrollToSkills}>
-            <h5>Skills</h5>
-          </li>
-          <li className='p-6 cursor-pointer' onClick={scrollToPortfolio}>
-            <h5>Portfolio</h5>
-          </li>
-          <li className='p-6 cursor-pointer' onClick={scrollToContact}>
-            <h5>Contact</h5>
-          </li>
-          <li className='p-6 cursor-pointer'>
-            <h5>Resume</h5>
-          </li>
+        <ol className='hidden md:flex h-full'>
+          <MenuItem title={'Home'} scrollTo={scrollToHome} />
+          <MenuItem title={'About'} scrollTo={scrollToAbout} />
+          <MenuItem title={'Skills'} scrollTo={scrollToSkills} />
+          <MenuItem title={'Portfolio'} scrollTo={scrollToPortfolio} />
+          <MenuItem title={'Contact'} scrollTo={scrollToContact} />
+          <MenuItem title={'Resume'} scrollTo={scrollToHome} />
         </ol>
         <div className='md:hidden space-y-2 cursor-pointer' onClick={toggleMenu}>
           <span className='block w-4 h-1 bg-gradient-to-r from-violet-500 to-fuchsia-500'></span>
