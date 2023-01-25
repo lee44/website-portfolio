551838debc238f8731ef93e865a2dbdec1dde7e2
lee44 <jlee7772@gmail.com>
2023-01-25 09:38:12 -0800

    Added animation for subtitle

--- components/title/SubTitle.js
+++ components/title/SubTitle.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 
 const SubTitle = ({ subtitle }) => {
   return (
-    <div className='flex flex-col items-center my-8'>
+    <motion.div
+      className='flex flex-col items-center my-8'
+      initial={{ y: -50, opacity: 0 }}
+      whileInView={{ y: 0, opacity: 1 }}
+      viewport={{ once: true }}
+      transition={{ ease: 'easeInOut', duration: 1 }}
+    >
       <h1 className='mb-0'>{subtitle}</h1>
       <hr className='w-16 h-2 bg-gradient-to-r from-violet-500 to-fuchsia-500 border-0'></hr>
-    </div>
+    </motion.div>
   )
 }
 
