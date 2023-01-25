60f618853984e9c5117e1b98f005951b915278e5
lee44 <jlee7772@gmail.com>
2023-01-25 10:21:43 -0800

    Added custom animation to card component

--- components/ui/card.js
+++ components/ui/card.js
@@ -1,7 +1,15 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+import { animations } from '../animation/animations'
 
 const Card = ({ children, classes }) => {
-  return <div className={`${classes} bg-primary-bg rounded-md p-5 border-[1px] border-primary`}>{children}</div>
+  const keys = Object.keys(animations)
+  const randomAnimation = animations[keys[Math.floor(Math.random() * 4)]]
+  return (
+    <motion.div className={`${classes} bg-primary-bg rounded-md p-5 border-[1px] border-primary`} {...randomAnimation}>
+      {children}
+    </motion.div>
+  )
 }
 
 export default Card
