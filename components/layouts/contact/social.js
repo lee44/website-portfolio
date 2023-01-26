63ea3fc7b96ddc2316035c9f39a59bd83f11bc9e
lee44 <jlee7772@gmail.com>
2023-01-25 20:01:13 -0800

    Fixed

---
+++ components/layouts/contact/social.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Card from '../../templates/card'
+import SocialBar from './socialbar'
+
+const Social = () => {
+  return (
+    <>
+      <Card classes={'flex flex-col max-w-lg md:justify-self-start'} animation={'fade-right'}>
+        <h3 className='text-primary my-0'>Say Hello Here</h3>
+        <hr />
+        <SocialBar direction={'vertical'} showLinks={true} />
+      </Card>
+    </>
+  )
+}
+
+export default Social
