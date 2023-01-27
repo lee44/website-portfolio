537f82a09f36f1858be8f382507b1cdf80b8294b
lee44 <jlee7772@gmail.com>
2023-01-27 15:08:00 -0800

    Added form property

--- components/layouts/contact/message.js
+++ components/layouts/contact/message.js
@@ -7,7 +7,7 @@ const Message = () => {
       <Card classes={'flex flex-col max-w-lg xl:justify-self-end min-w-0'} animation={'fade-left'}>
         <h3 className='text-primary my-0'>Send Me A Message</h3>
         <hr />
-        <form className='w-full' action='/' method='POST' data-netlify='true'>
+        <form name='contact' className='w-full' method='POST' data-netlify='true'>
           <input className='w-full p-2 bg-white rounded-md text-black' placeholder='Name' name='name' type='text' required />
           <input className='w-full p-2 my-3 bg-white rounded-md text-black' placeholder='Email' name='email' type='email' required />
           <textarea className='w-full p-2 bg-white rounded-md text-black' placeholder='Message' name='message' cols={5} rows={5} />
