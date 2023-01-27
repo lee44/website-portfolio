e37e2c113874b64841c85e28f3f6c26e0c8b53ef
lee44 <jlee7772@gmail.com>
2023-01-27 14:49:34 -0800

    Added netlify form

--- components/layouts/contact/message.js
+++ components/layouts/contact/message.js
@@ -7,7 +7,7 @@ const Message = () => {
       <Card classes={'flex flex-col max-w-lg xl:justify-self-end min-w-0'} animation={'fade-left'}>
         <h3 className='text-primary my-0'>Send Me A Message</h3>
         <hr />
-        <form className='w-full'>
+        <form className='w-full' action='/' method='POST' data-netlify='true'>
           <input className='w-full p-2 bg-white rounded-md' placeholder='Name' type='text' />
           <input className='w-full p-2 my-3 bg-white rounded-md' placeholder='Email' type='email' />
           <textarea className='w-full p-2 bg-white rounded-md' placeholder='Message' cols={5} rows={5} />
