180862dd5656c5df30d28172e53950eaf85fcf5a
lee44 <jlee7772@gmail.com>
2023-01-26 17:01:39 -0800

    On mobile horizontal scrollbar is fixed but vertical scrollbar still works when menu is up

--- components/layouts/nav/navigation.js
+++ components/layouts/nav/navigation.js
@@ -9,9 +9,11 @@ const Nav = ({ menuItems, scrollToHome }) => {
 
   useEffect(() => {
     if (menu) {
-      document.body.style.overflow = 'hidden'
+      document.body.style.overflowX = 'hidden'
+      document.body.style.overflowY = 'hidden'
     } else {
-      document.body.style.overflow = 'auto'
+      document.body.style.overflowX = 'hidden'
+      document.body.style.overflowY = 'auto'
     }
   }, [menu])
 
