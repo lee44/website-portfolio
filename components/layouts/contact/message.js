cca9c4cabece6d77b056027398c1f51e304ef943
lee44 <jlee7772@gmail.com>
2023-01-27 14:58:12 -0800

    Added submit button

--- components/layouts/contact/message.js
+++ components/layouts/contact/message.js
@@ -8,9 +8,14 @@ const Message = () => {
         <h3 className='text-primary my-0'>Send Me A Message</h3>
         <hr />
         <form className='w-full' action='/' method='POST' data-netlify='true'>
-          <input className='w-full p-2 bg-white rounded-md' placeholder='Name' type='text' />
-          <input className='w-full p-2 my-3 bg-white rounded-md' placeholder='Email' type='email' />
-          <textarea className='w-full p-2 bg-white rounded-md' placeholder='Message' cols={5} rows={5} />
+          <input className='w-full p-2 bg-white rounded-md text-black' placeholder='Name' type='text' />
+          <input className='w-full p-2 my-3 bg-white rounded-md text-black' placeholder='Email' type='email' />
+          <textarea className='w-full p-2 bg-white rounded-md text-black' placeholder='Message' cols={5} rows={5} />
+          <input
+            className='py-3 w-full rounded-md text-center font-bold text-lg flex-auto bg-button hover:bg-button-hover'
+            type='Submit'
+            value='Submit'
+          />
         </form>
       </Card>
     </>
