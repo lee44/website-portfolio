11d7e6253f94690c7f8e22e135fc4220a83ad178
lee44 <jlee7772@gmail.com>
2024-09-30 13:07:33 -0700

    Finished updating info

--- components/layouts/aboutme/aboutme.tsx
+++ components/layouts/aboutme/aboutme.tsx
@@ -8,7 +8,7 @@ const AboutMe = () => {
     <>
       <SubTitle subtitle={'About Me/Goals'} />
       <Card classes={'mb-20'} animation={'fade-up'}>
-        I graduated from UC Santa Cruz with a B.S in Computer Science. Currently I work at a fashion company, CLO Virtual Fashion Inc, as a web developer for over two years. Unfortunately I do not see long term growth within the company so I am searching for a company that can offer growth and further move me along my career.
+        I graduated from UC Santa Cruz with a B.S in Computer Science. Currently I work at a fashion company, CLO Virtual Fashion Inc, as a web developer for over two years. Unfortunately I do not see long term growth within the company so I am searching for a company that can offer growth and further move me along my career. During my free time, I continue to sharpen my web development skills by building beautiful, responsive websites using frameworks like NextJS, ReactJS, and TailwindCSS.
       </Card>
       <Timeline />
     </>
